15beb236bfe2cfe36107f606bb4bf9fa2f5dee4a
Max98002 <maksimka98002@gmail.com>
2019-11-06 17:26:52 +0200

    add modal service

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
   var modal = $('#modal');
   var modal__button = $('.modal-btn');
   var modal__close = $('.modal__close');
+  var services__button = $('.services_btn');
+  var modal__services = $('#servicesModal');
+  var modal_servicesClose = $('.servicesModal-close');
 
   modal__button.on('click', function (event) {
     event.preventDefault();
@@ -10,6 +13,16 @@ $(document).ready(function () {
   modal__close.on('click', function () {
     modal.removeClass('modal-active');
   });
+  // Modal-services-active
+  services__button.on('click', function (eventm) {
+    eventm.preventDefault();
+    modal__services.addClass('servicesModal-active');
+  });
+  modal_servicesClose.on('click', function () {
+    modal__services.removeClass('servicesModal-active');
+  });
+  // Modal-services-active
+
 
   // Прокрутка До секцій
   $("a[href^='#']").click(function () {
@@ -84,5 +97,57 @@ $(document).ready(function () {
     }
   });
   // Валідація форми
+
+  // Валідація форми Service
+  $('#service-validate').validate({
+    rules: {
+      field: { accept: "[a-zA-Z]+" },
+      username: {
+        required: true,
+        minlength: 2,
+        maxlength: 8,
+      },
+      email: {
+        required: true,
+        email: true
+      }
+    },
+    errorElement: "div",
+    errorClass: "error",
+    messages: {
+      username: {
+        required: "Вкажіть ім'я",
+        minlength: jQuery.validator.format("Залишилось {0} символи"),
+        maxlength: jQuery.validator.format("Не більше {0} символів")
+      },
+      email: {
+        required: "Заповніть поле Email",
+        email: "Введіть коректний Email"
+      }
+    },
+    submitHandler: function (form) {
+      $.ajax({
+        url: form.action,
+        type: form.method,
+        data: $(form).serialize(),
+        success: function (response) {
+          console.log("Hello Все гуд: " + response);
+          $('.modal-form')[0].reset();
+          modal__services.removeClass('servicesModal-active');
+          var thanks = $('#thanks');
+          thanks.addClass('thanks-active');
+          function close() {
+            thanks.removeClass('thanks-active');
+          }
+          setTimeout(close, 3000);
+        },
+        error: function (jqHXR, textStatus, errorThrown) {
+          console.log(jqHXR + " " + textStatus);
+        }
+      });
+    }
+  });
+  // Валідація форми Service
+
 
 });
